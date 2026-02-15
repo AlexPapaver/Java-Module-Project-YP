44c1ec8b880dff8421635244010242e16a012cca
Alex <rodo.kct@gmail.com>
2026-02-15 15:25:56 +0300

    Первая рабочая версия

---
+++ src/main/java/Race.java
@@ -0,0 +1,14 @@
+public class Race {
+
+    public static void winner(Car[] cars) {
+
+        int m = 0;
+        for (int i = 1; i <= (cars.length - 1); i++) {
+            if (cars[i].velocity > cars[i - 1].velocity) {
+                m = i;
+            }
+        }
+        System.out.println("Победитель: " + cars[m].name);
+
+    }
+}
